f0be89f7da521bfd3a18ce745b2b05b873564b3d
Paul BarrettJr <pba243@cs.uky.edu>
2018-04-23 20:10:38 -0400

    completed kruskalTraverse.cc, updated readme

--- README.md
+++ README.md
@@ -9,20 +9,23 @@ input: consists of n+1 lines. The first line includes n, the size of undirected 
 output: the first line is the weight (length) of the MST T. The second line is n, the size of the matrix, and it is followed by n lines representing T
 
 kruskalTraverse.cc:
-input: A n x n distance matrix M representing a weighted tree T with n vertices, and the starting vertex v
+input: A n x n distance matrix M representing a weighted tree T with n vertices, and the starting vertex v is given as a command line argument
 output: list of vertices of T visited in preorder, starting at v
 sample input:
+
 4	//weight of t
 4	//# of lines in matrix of MST
 0 0 2 0 //matrix
 0 0 0 1
 2 0 0 1
 0 1 1 0
-3	//starting vertex
+
 sample output:
 3 1 4 2
 
 The output from kruskalMinSpanTree.cc will be used as the input for kruskalTraverse.cc in order to use the minimum spanning tree to create the depth first search traversal. This will be done using piping in the linux terminal.
+
+Sample execution: $ ./kruskalMinSpanTree | ./kruskalTraverse 3
 
 code modified from original at:
 https://github.com/luisfcofv/competitive-programming-book/blob/master/ch4/ch4_03_kruskal_prim.cpp
--- kruskalTraverse.cc
+++ kruskalTraverse.cc
@@ -1,47 +1,108 @@
 /*
 Paul Barrett
 FILE: kruskalTraverse.cc
-DESCRIPTION: Given output in the form of an adjacency matrix for a MST from kruskalMinSpanTree.cc
-and a vertext to begin at, uses depth first search to find the preorder traversal for the tree
-USE: ./kruskalMinSpanTree | ./kruskalTraverse
+DESCRIPTION: Given output in the form of an adjacency matrix for a MST from kruskalMinSpanTree.cc and a vertext to begin at, uses depth first search to find the preorder traversal for the tree
+
+USE: $ ./kruskalMinSpanTree | ./kruskalTraverse [starting vertex]
+input format:
+4	<= number of vertices
+0 2 2 6 <= adjacency matrix
+2 0 5 1
+2 5 0 1
+6 1 1 0
+
+note: you can also use just $ ./kruskalTraverse [starting vertex] and input with format:
+4	<= cost of tree
+4	<= number of vertices
+0 0 2 0 <= adjacency matrix of MST
+0 0 0 1 
+2 0 0 1 
+0 1 1 0 
+
 COMPILATION: g++ -o kruskalTraverse kruskalTraverse.cc
 */
 
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include <list>
 using namespace std;
 
-vector <int> remembervisited;
-bool visited(int rowv){
-	bool remembered = false;
-	for(int v = 0; v < remembervisited.size(); v++){
-		if (remembervisited[v] == rowv)
-			remembered = true;
-	}
-	if (remembered == false)
-		remembervisited.push_back(rowv);
-	return remembered;
+// Graph class represents a directed graph
+// using adjacency list representation
+// this code was borrowed from:
+// https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+// and modified to work correctly with new input
+class Graph
+{
+    int V;    // No. of vertices
+ 
+    // Pointer to an array containing
+    // adjacency lists
+    list<int> *adj;
+ 
+    // A recursive function used by DFS
+    void DFSUtil(int v, bool visited[]);
+public:
+    Graph(int V);   // Constructor
+ 
+    // function to add an edge to graph
+    void addEdge(int v, int w);
+ 
+    // DFS traversal of the vertices
+    // reachable from v
+    void DFS(int v);
+};
+ 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
 }
-
-void DFS(vector <int> rowvec, int vertex, vector <int> &destination){
-	visited(vertex);
-	destination.push_back(vertex);
-	for (int k = 0; k < rowvec.size(); k++){
-		if (visited(rowvec[k]) == false)
-			DFS(rowvec, rowvec[k], destination);
-	}
+ 
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); // Add w to v’s list.
+}
+ 
+void Graph::DFSUtil(int v, bool visited[])
+{
+    // Mark the current node as visited and
+    // print it
+    visited[v] = true;
+    cout << v+1 << " ";
+ 
+    // Recur for all the vertices adjacent
+    // to this vertex
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFSUtil(*i, visited);
+}
+ 
+// DFS traversal of the vertices reachable from v.
+// It uses recursive DFSUtil()
+void Graph::DFS(int v)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Call the recursive helper function
+    // to print DFS traversal
+    DFSUtil(v, visited);
 }
 
 int main(int argc, char *argv[]){
 
 if (argc != 2){
-	cout << "Specify start vertex: ./kruskalTravers [start vertex]" << endl;
+	cout << "Specify start vertex: ./kruskalTraverse [start vertex]" << endl;
 	return 1;
 }
 int numverts;
 int weight;
-int startvert = atoi(argv[1]) - 1;
+int startvert;
 int edgevalue;
 vector <int> rowvector;
 vector <int> colvector;
@@ -50,29 +111,19 @@ cin >> weight >> numverts;
 for (int row = 0; row < numverts; row++){
 	for (int col = 0; col < numverts; col++){
 		cin >> edgevalue;
-		if (edgevalue != 0 && col > row){
+		if (edgevalue != 0){
 			rowvector.push_back(row);
 			colvector.push_back(col);
 		}
 	}	
 }
-rowvector.push_back(numverts);
-/*for (int j = 0; j < numverts*numverts - 1; j++){
-	cin >> edgevalue;
-}*/
-
-/*cout << "startvert = "<<startvert;
-cout << endl;
-
-for (int i = 0; i < numverts-1; i++){
-                cout << "(" << rowvector[i] << ", " <<
-		colvector[i] << ")" << endl;
-}*/
-vector <int> preorder;
-DFS(rowvector, startvert, preorder);
-for(int t = 0; t < numverts; t++){
-	cout << preorder[t] + 1 << endl;
+Graph g(numverts);
+for (int i = 0; i < 2*numverts - 2; i++){
+	g.addEdge(rowvector[i], colvector[i]);
 }
+
+g.DFS(startvert);
+cout << endl;
 
 return 0;
 }
